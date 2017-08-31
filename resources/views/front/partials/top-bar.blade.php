feba2103c4112bb0cb2e19dd5cdb43a8f8b10db3
Kevin Ghio <ghiokevin@gmail.com>
2017-08-30 21:46:41 -0300

    styles

--- resources/views/front/partials/top-bar.blade.php
+++ resources/views/front/partials/top-bar.blade.php
@@ -1,21 +1,25 @@
-<div class="top-bar hidden-xs">
+<div class="top-bar">
+
+	<button type="button" class="hamburguesa navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+     </button>
     <!-- Nested Container Starts -->
     <div class="container">
         <!-- Nested Row Starts -->
         <div class="row">
-			<div class="col-md-2  col-sm-6 col-xs-12">
+			<div class="col-md-2  col-sm-6 col-xs-3">
 				<a href="/" class="logo-diselo">
-					<img src="/images/front/logo-diselo.svg" alt="Diselo" >
+					<img src="/images/front/logo-diselo.svg" alt="Diselo" class="img-responsive" >
 				</a>
 			</div>
 			<!-- Navbar Cat collapse Starts -->
-	        <div class="col-md-6 collapse navbar-collapse navbar-cat-collapse">
-	            <ul class="navbar navbar-nav">
+	        <div class="col-md-6 collapse navbar-collapse navbar-cat-collapse" id="navbar">
+	            <ul class="navbar navbar-nav my-nav" >
 
 	                @foreach ($categories as $category)
 	                    @if ($category->subcategories->count())
 	                        <li class="dropdown">
-	                            <a href="{{$category->slug}}" class="dropdown-toggle" data-hover="dropdown" data-delay="10">{{$category->name}}</a>
+	                            <a href="/{{$category->slug}}" class="dropdown-toggle" data-hover="dropdown" data-delay="10">{{$category->name}}</a>
 	                            <ul class="dropdown-menu" role="menu">
 	                                @foreach ($category->subcategories as $subcategory)
 	                                    <li><a tabindex="-1" href="/{{$category->slug}}/{{$subcategory->slug}}">{{$subcategory->name}}</a></li>
@@ -31,18 +35,18 @@
 	        </div>
             <!-- Shopping Cart Starts -->
             @php $items = (session()->has('cart')) ? count(session('cart')) : 0;  @endphp
-            <div class="col-md-2 col-sm-6 col-xs-12 cart">
+            <div class="col-md-2 col-sm-6 col-xs-3 cart">
                 {{-- <div id="cart" class="btn-group btn-block"> --}}
                 <div id="cart" class="">
                     <a href="/carrito" class="btn btn-block btn-lg text-uppercase">
                         <i class="fa fa-shopping-cart"></i>
-                        <span id="cart-total">{{$items}} item(s)</span>
+                        <span id="cart-total">{{$items}} <em class="hidden-xs">item(s)</em></span>
                     </a>
                 </div>
             </div>
             <!-- Shopping Cart Ends -->
 			<!-- Search Starts -->
-			<div class="col-md-3 col-sm-6 col-xs-12 search">
+			<div class="col-md-3 col-sm-6 col-xs-6 search">
 				<form class="search" action="/productos/search" method="get">
 					<div id="search">
 						<div class="input-group">
--- storage/framework/views/313112dd7c1660598a50ff947350a0339eb8b326.php
+++ storage/framework/views/313112dd7c1660598a50ff947350a0339eb8b326.php
@@ -1,21 +1,25 @@
-<div class="top-bar hidden-xs">
+<div class="top-bar">
+
+	<button type="button" class="hamburguesa navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false">
+        <i class="fa fa-bars" aria-hidden="true"></i>
+     </button>
     <!-- Nested Container Starts -->
     <div class="container">
         <!-- Nested Row Starts -->
         <div class="row">
-			<div class="col-md-2  col-sm-6 col-xs-12">
+			<div class="col-md-2  col-sm-6 col-xs-3">
 				<a href="/" class="logo-diselo">
-					<img src="/images/front/logo-diselo.svg" alt="Diselo" >
+					<img src="/images/front/logo-diselo.svg" alt="Diselo" class="img-responsive" >
 				</a>
 			</div>
 			<!-- Navbar Cat collapse Starts -->
-	        <div class="col-md-6 collapse navbar-collapse navbar-cat-collapse">
-	            <ul class="navbar navbar-nav">
+	        <div class="col-md-6 collapse navbar-collapse navbar-cat-collapse" id="navbar">
+	            <ul class="navbar navbar-nav my-nav" >
 
 	                <?php $__currentLoopData = $categories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $category): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
 	                    <?php if($category->subcategories->count()): ?>
 	                        <li class="dropdown">
-	                            <a href="<?php echo e($category->slug); ?>" class="dropdown-toggle" data-hover="dropdown" data-delay="10"><?php echo e($category->name); ?></a>
+	                            <a href="/<?php echo e($category->slug); ?>" class="dropdown-toggle" data-hover="dropdown" data-delay="10"><?php echo e($category->name); ?></a>
 	                            <ul class="dropdown-menu" role="menu">
 	                                <?php $__currentLoopData = $category->subcategories; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $subcategory): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
 	                                    <li><a tabindex="-1" href="/<?php echo e($category->slug); ?>/<?php echo e($subcategory->slug); ?>"><?php echo e($subcategory->name); ?></a></li>
@@ -31,18 +35,18 @@
 	        </div>
             <!-- Shopping Cart Starts -->
             <?php  $items = (session()->has('cart')) ? count(session('cart')) : 0;   ?>
-            <div class="col-md-2 col-sm-6 col-xs-12 cart">
+            <div class="col-md-2 col-sm-6 col-xs-3 cart">
                 
                 <div id="cart" class="">
                     <a href="/carrito" class="btn btn-block btn-lg text-uppercase">
                         <i class="fa fa-shopping-cart"></i>
-                        <span id="cart-total"><?php echo e($items); ?> item(s)</span>
+                        <span id="cart-total"><?php echo e($items); ?> <em class="hidden-xs">item(s)</em></span>
                     </a>
                 </div>
             </div>
             <!-- Shopping Cart Ends -->
 			<!-- Search Starts -->
-			<div class="col-md-3 col-sm-6 col-xs-12 search">
+			<div class="col-md-3 col-sm-6 col-xs-6 search">
 				<form class="search" action="/productos/search" method="get">
 					<div id="search">
 						<div class="input-group">
